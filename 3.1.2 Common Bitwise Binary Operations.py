5399dd36a61b9ca773b0a7a7fa935f18772f2fc8
interpreterhegel <interpreterhegel@gmail.com>
2024-02-26 15:26:12 +0800

    Update 3.1.2 Common Bitwise Binary Operations.py

--- 3.1.2 Common Bitwise Binary Operations.py
+++ 3.1.2 Common Bitwise Binary Operations.py
@@ -16,6 +16,53 @@ print("x&(x-1):bin: {}".format(bin(x&(x-1))))
 # Additional info: the above algo is used to test if a number is the exponential of two.
 
 # 2. Locate the rightmost 1
-
+# print("~(x-1):bin: {}".format(bin(~(x-1))))
+# print("x&~(x-1):dec: {}".format(x&~(x-1)))
+# print("x&~(x-1):bin: {}".format(bin(x&~(x-1))))
 
 # 3. Swap bits
+# Function definition
+def swapBits (n, p1, p2):
+    #print("function called")
+    print ("Task: swap bits (LSB at 0)")
+    print ("Exchange the bits at location {0} and {1} of integer {2}".format(p1, p2, n))
+    print ("Original binary form: {}".format(bin(n)))
+    # Extract the two bits required using &1
+    bit_1 = (n >> p1) & 1
+    bit_2 = (n >> p2) & 1
+    print ("Extract to get :")
+    print ("bit_1: {}".format(bin(bit_1)))
+    print ("bit_2: {}".format(bin(bit_2)))
+    # Use ^ to exchange bits.
+    # Leftshift the a^b by p1 units.
+    # Bitwise xor with n s.t. only the bit at p1 is replaced.
+    temp = bit_1 ^ bit_2
+    n = n ^ (temp << p1)
+    print ("Keep left p2 and replace right p1 to get")
+    print ("Binary: {0}, decimal: {1}".format(n, bin(n)))
+    n = n ^ (temp << p2)
+    print ("Keep the modified p1 and replace the left p2 to get")
+    print ("Binary: {0}, decimal: {1}".format(n, bin(n)))
+    print ("which is our final result.")
+
+# Driver code
+res = swapBits (28, 0, 3)
+print("Result = ", res)
+
+# Below are the most important two bit manipulations.
+"""
+& i.e. bitwise and
+for any bit A, A & 1 gives A itself, A & 0 gives 0.
+However, if A contains more than 1 bit, 
+A & 0001 is giving the LSB.
+"""
+
+"""
+ ^ i.e. bitsise xor, different = 1, same = 0.
+for any pairs of single bits A and B,
+A^0 gives A itself. A^A gives 0.
+"""
+
+
+
+
